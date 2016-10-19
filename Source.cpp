7165910081a4eb6ff9124c6c014d34dc772daec1
Daantjuh77 <j.d.v.d.klooster@student.tue.nl>
2016-10-19 09:19:12 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,36 @@
 #include <stdio.h>
-#include <math.h>
 
-#define size 8
+bool NextSymbol(bool BusData, int BusDataOld){
+	bool Status;
+	if (BusData != BusDataOld){
+		Status = true;
+	}
+	else{
+		Status = false;
+	}
+	return Status;
+}
 
 void main(void){
-	int Binary[size] = {};
-	int b[size] = {};
-	scanf_s("%d", &b[0]);
-	printf("Scanned number: %d\n", b[0]);
-	for (int i = 0; i < size;i++){
-		b[i+1] = b[i] >> 1;
-		if (b[i] == 0){
-			Binary[i] = 0;
+	bool BusData=true;
+	bool BusDataOld=true;
+	bool Check;
+	int j=0, i=0;
+	for (i = 0; i<5; i++){
+		if (i < 3){
+			BusData = true;
 		}
-		else if((b[i] - b[i + 1]) != b[i+1]){
-			Binary[i] = 1;
+		else if (i >= 3){
+			BusData = false;
 		}
-		else{
-			Binary[i] = 0;
-		}		
+		Check = NextSymbol(BusData, BusDataOld);
+		if (Check == true){
+			j = 1;
+		}
+		else if (Check == false){
+			j = 0;
+		}
+		printf("i=%d		Check=%d\n", i, j);
 	}
-	printf("Binary number: ");
-	for (int i = 0; i < size; i++){
-		printf("%d", Binary[(size-1)-i]);
-	}
+	BusDataOld = BusData;
 }
