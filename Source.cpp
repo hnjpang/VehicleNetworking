cf5971324358f4eee0aa1b955a9cf8e7a0c78626
Daantjuh77 <j.d.v.d.klooster@student.tue.nl>
2016-10-19 09:14:25 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
-#define RxFrame 3
+#define size 8
 
 void main(void){
-	int ID[8] = {};
-	int binID = 0;
-	int c[8] = {};
-	int BusData[8] = { 0, 0, 0, 0, 1, 0, 1, 0 };
-	int Frame[RxFrame] = {};
-	int fullData=0;
-	for (int j = 0; j < RxFrame; j++){
-		for (int i = 0; i<8; i++){
-				if (BusData[i] == 0){
-					ID[i] = 0;
-				}
-				else if (BusData[i] == 1){
-					ID[i] = 1;
-				}
-			if (i == 0){
-				binID = ID[0];
-				c[i] = binID;
-			}
-			else{
-				binID = (binID << 1) + ID[i];
-				c[i] = binID;
-			}
+	int Binary[size] = {};
+	int b[size] = {};
+	scanf_s("%d", &b[0]);
+	printf("Scanned number: %d\n", b[0]);
+	for (int i = 0; i < size;i++){
+		b[i+1] = b[i] >> 1;
+		if (b[i] == 0){
+			Binary[i] = 0;
 		}
-		Frame[j] = binID;
+		else if((b[i] - b[i + 1]) != b[i+1]){
+			Binary[i] = 1;
+		}
+		else{
+			Binary[i] = 0;
+		}		
 	}
-	for (int h = RxFrame - 1; h >= 0; h--){
-		fullData = fullData + (Frame[h] << (8 * h));
-	}
-	for (int i = 0; i < 8; i++){
-		printf("RxFrame=%d, fullData=%d and binID=%d, Frame=%d\n", binID, fullData, c[i], Frame[i]);
+	printf("Binary number: ");
+	for (int i = 0; i < size; i++){
+		printf("%d", Binary[(size-1)-i]);
 	}
 }
